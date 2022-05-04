b5c6bbc370c5ae16afc3d31a02905331c1f7d432
minsku <minja98@hotmail.com>
2022-05-04 21:07:03 +0300

    logout

--- src/views/Logout.js
+++ src/views/Logout.js
@@ -1,11 +1,15 @@
+import React, {useEffect} from 'react';
 import {useContext} from 'react';
 import {Navigate} from 'react-router-dom';
 import {MediaContext} from '../contexts/MediaContext';
 
 const Logout = () => {
-  const [user, setUser] = useContext(MediaContext);
-  setUser(null);
-  localStorage.clear();
+  const {user, setUser} = useContext(MediaContext);
+  useEffect(() => {
+    setUser(null);
+    localStorage.clear();
+  }, []);
+
   return <>{!user ? <Navigate to="/" /> : <div>Loading...</div>}</>;
 };
 
